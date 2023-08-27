bfffd2db10011a7c73e1ffe4f7d2d8997d9c3497
Corey <Corey255A1@hotmail.com>
2023-08-27 14:53:39 -0400

    Initial Commit of some stuff.. need to figure out what else to commit

---
+++ Source/MusicPlayer/MusicGameBase.h
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MusicGameBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MUSICPLAYER_API AMusicGameBase : public AGameModeBase
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly)
+	FString APIKey;
+
+	UPROPERTY(EditDefaultsOnly)
+	FString ServerURL;
+
+	virtual void BeginPlay() override;
+
+	virtual void Tick(float DeltaTime) override;
+};
